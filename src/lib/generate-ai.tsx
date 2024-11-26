31ffb008497c1cce3f370741ab6f842d9a09ee1e
ricardoabicalhob <rbicalho2014@gmail.com>
2024-11-25 23:42:22 -0300

    teste

--- src/lib/generate-ai.tsx
+++ src/lib/generate-ai.tsx
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 export async function GenerateAI( req :string) {
 
     const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY
-    const genAI = new GoogleGenerativeAI(apiKey)
+    const genAI = new GoogleGenerativeAI(apiKey as string)
 
 
     try {
