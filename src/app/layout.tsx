5b49046faf1b2b9d3e8918ffd44d4492adb546c4
ricardoabicalhob <rbicalho2014@gmail.com>
2024-11-28 14:38:03 -0300

    outra vez.....

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-[calc(100vh - 60px)]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
         {children}
       </body>
