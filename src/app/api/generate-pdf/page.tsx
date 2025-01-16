e0a7a231856aa4de8664f550ed7075d3ea4f8eac
ricardoabicalhob <rbicalho2014@gmail.com>
2025-01-16 15:24:37 -0300

    outra vez

--- src/app/api/generate-pdf/page.tsx
+++ src/app/api/generate-pdf/page.tsx
@@ -7,32 +7,32 @@ import { useContext } from "react";
 // @ts-expect-error erro ja esperado
 export default async function GeneratePdf(req, res :NextApiResponse) {
 
-        function uint8ArrayToBase64(array :Uint8Array) {
-            // @ts-expect-error erro já previsto array
-            return btoa(String.fromCharCode.apply(null, array)); 
-        }
+        // function uint8ArrayToBase64(array :Uint8Array) {
+        //     // @ts-expect-error erro já previsto array
+        //     return btoa(String.fromCharCode.apply(null, array)); 
+        // }
 
-        const browser = await puppeteer.launch()
-        const page = await browser.newPage()
+        // const browser = await puppeteer.launch()
+        // const page = await browser.newPage()
 
-        await page.goto('http://ricardoabicalhob.github.io/segurancanotrabalho/editor')
-        // await page.goto('http://localhost:3000/relatorio')
+        // await page.goto('http://ricardoabicalhob.github.io/segurancanotrabalho/editor')
+        // // await page.goto('http://localhost:3000/relatorio')
         
 
-        const pdfBuffer = await page.pdf({
-            format: 'A4',
-            printBackground: true,
-            margin: {top: '1cm', right: '1cm', bottom: '1cm', left: '1cm'}
-        })
+        // const pdfBuffer = await page.pdf({
+        //     format: 'A4',
+        //     printBackground: true,
+        //     margin: {top: '1cm', right: '1cm', bottom: '1cm', left: '1cm'}
+        // })
 
-        const base64Buffer = uint8ArrayToBase64(pdfBuffer)
-        const dataUrl = `data:application/pdf;base64,${base64Buffer}`       
+        // const base64Buffer = uint8ArrayToBase64(pdfBuffer)
+        // const dataUrl = `data:application/pdf;base64,${base64Buffer}`       
 
-        await browser.close()
+        // await browser.close()
 
-        res?.setHeader('Content-Type', 'application/pdf')
-        //res?.setHeader('Content-Disposition', 'attachment; filename="meu-pdf.pdf"')
-        res?.send(base64Buffer)
+        // res?.setHeader('Content-Type', 'application/pdf')
+        // //res?.setHeader('Content-Disposition', 'attachment; filename="meu-pdf.pdf"')
+        // res?.send(base64Buffer)
 
         return(
             <html lang="pt-BR">
@@ -42,7 +42,8 @@ export default async function GeneratePdf(req, res :NextApiResponse) {
             </head>
             <body>  
                 <div className="h-screen w-screen">
-                    <iframe src={dataUrl} width="100%" height="100%"></iframe>
+                    {/* <iframe src={dataUrl} width="100%" height="100%"></iframe> */}
+                    <iframe src="" width="100%" height="100%"></iframe>
                 </div>
             </body>
             </html>
